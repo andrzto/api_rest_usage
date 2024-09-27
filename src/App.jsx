dc5ed18abc7ee4acf7a0e24a428746fe60f81233
andrzto <60622479+andrzto@users.noreply.github.com>
2024-09-27 18:23:10 -0500

    component AppiCoffeeCarruselBbasic

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import { AppiCoffeeCarruselBasic } from './assets/components/AppiCoffeeCarruselBasic'
+
+function App() {
+
+
+  return (
+    <div>
+      <AppiCoffeeCarruselBasic />
+    </div>
+  )
+}
+
+export default App
